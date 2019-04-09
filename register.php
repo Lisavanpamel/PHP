c760013fec7d027ecf0c0363b3c095770c327076
ElineVDB <eline.vandenborren@telenet.be>
2019-04-09 13:01:45 +0200

    Create register.php

---
+++ register.php
@@ -0,0 +1,64 @@
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Sign in</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <header>
+  <h1>Sign in</h1>
+  </header>
+
+  <form method="post" action="">
+    <!-- username -->
+    <div class="input">
+    <label>Username</label>
+    <input type="text"name="username">
+  </div>
+
+  <!-- e-mail -->
+  <div class="input">
+    <label>E-mail</label>
+    <input type="text"name="email">
+  </div>
+
+  <!-- birthdate -->
+  <div class="input">
+    <label>Birthdate</label>
+    <input type="date"name="birthdate">
+  </div>
+
+  <!-- country -->
+    <div class="input">
+      <label>Country</label>
+    </div>
+
+    <!-- password -->
+    <div class="input">
+      <label>Password</label>
+      <input type="password"name="password">
+    </div>
+    <!-- confirm password -->
+    <div class="input">
+      <label>Confirm Password</label>
+      <input type="password"name="password_confirm">
+    </div>
+
+      <!-- submit button -->
+      <input type="submit" value="Sign in">
+  </form>
+
+</body>
+
+
+</html>
