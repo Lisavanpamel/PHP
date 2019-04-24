0b230091e038805bd40aa2ee2a711ad7e20ecd0a
davyceuppens <davy.ceuppens@hotmail.be>
2019-04-24 22:54:58 +0200

    CSS
    Wat css aangepast, font,posts en btn

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $posts = $post->getPosts();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
@@ -16,8 +17,9 @@ $posts = $post->getPosts();
 
     <?php while($row = $posts->fetch()) : ?>
         <div class="post" data-id="<?php echo $row['id']?>">
+        <!--User id: naam n profiel foto evt weergeven -->
             <img class= "img" src="<?php echo $row['post_img'] ?>" alt="post_img" height="auto" width="60px">    
-            <p><?php echo $row['description'] ?></p>
+            <p class="description"><?php echo $row['description'] ?></p>
             </div>
         </div>
     <?php endwhile; ?>
