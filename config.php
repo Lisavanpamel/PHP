d4d31e6db0866cfd9cdc47e00252ad0454cba6ab
Jens De Landtsheer <49272464+Jenz99@users.noreply.github.com>
2019-04-25 17:50:49 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,16 @@
    define('DB_PASSWORD', 'root');
    define('DB_DATABASE', 'php2019');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
+
+   $host = "localhost"; /* Host name */
+   $user = "root"; /* User */
+   $password = "root"; /* Password */
+   $dbname = "php2019"; /* Database name */
+
+   $con = mysqli_connect($host, $user, $password,$dbname);
+
+   // Check connection
+   if (!$con) {
+   die("Connection failed: " . mysqli_connect_error());
+   }
+   ?>
