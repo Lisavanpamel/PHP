f7d35ede34374b2cc724e28089ea6f1dc8ffaec8
Jens De Landtsheer <49272464+Jenz99@users.noreply.github.com>
2019-05-09 11:42:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $posts = $post->getPosts();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>PHP Project</title>
 </head>
 <body>
 
@@ -25,33 +25,18 @@ $posts = $post->getPosts();
         <!--User id: naam n profiel foto evt weergeven -->
             <img class= "img" src="<?php echo $row['post_img'] ?>" alt="post_img" height="auto" width="60px">
             <p class="description"><?php echo $row['description'] ?></p>
-
-
-
-
-        <!--Likes -->
-        <?php
-        // determine if user has already liked this post
-        $con = mysqli_connect('localhost', 'root', 'root', 'php2019');
-        $results = mysqli_query($con, "SELECT * FROM likes WHERE userid=1 AND postid=".$row['id']."");
-
-        if (mysqli_num_rows($results) == 1 ): ?>
+            
             <!-- user already likes post -->
-            <span class="unlike fa fa-thumbs-up" data-id="<?php echo $row['id']; ?>"></span>
+			<span class="unlike fa fa-thumbs-up" data-id="<?php echo $row['id']; ?>"></span> 
             <span class="like hide fa fa-thumbs-o-up" data-id="<?php echo $row['id']; ?>"></span>
-        <?php else: ?>
             <!-- user has not yet liked post -->
-            <span class="like fa fa-thumbs-o-up" data-id="<?php echo $row['id']; ?>"></span>
-            <span class="unlike hide fa fa-thumbs-up" data-id="<?php echo $row['id']; ?>"></span>
-        <?php endif ?>
-
-        <span class="likes_count"><?php echo $row['likes']; ?> likes</span>
+			<span class="like fa fa-thumbs-o-up" data-id="<?php echo $row['id']; ?>"></span> 
+            <span class="unlike hide fa fa-thumbs-up" data-id="<?php echo $row['id']; ?>"></span>  
+            <span class="likes_count"><?php echo $row['likes']; ?> likes</span>
 
     </div>
     <?php endwhile; ?>
-
-
-
+    
     <button class="show-posts">Show more</button>
 </section>
 
